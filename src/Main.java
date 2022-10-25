006250675399bf6cb1aededb428ec9547edd512c
Рустам <rustam19871203@gmail.com>
2022-10-25 23:40:00 +0400

    Методы. Домашка

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    public static void leapYear(int years) {
+        if (years % 4 == 0 && years % 100 != 0 || years % 400 == 0) {
+            System.out.println(years + " год является високосным");
+        } else System.out.println(years + "год не является високосным");
+    }
+
+    private static void osForClientDevise(int clientOs, int clientDeviceYear) {
+        if (clientOs == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOs == 0) {
+            System.out.println("Установите облегченную версию приложения iOS");
+        }
+        if (clientOs == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }else if (clientOs == 1) {
+            System.out.println("Установите облегченную версию приложения Android");
+        }
+    }
+
+    public static void deliveryOfBankingProducts(int deliveryDistance) {
+        if (deliveryDistance <= 20) {
+            System.out.println("Доставка в пределах " + deliveryDistance + " км занимает сутки.");
+        } else if (deliveryDistance <= 60) {
+            System.out.println("Доставка в пределах " + deliveryDistance + " км занимает двое суток.");
+        } else if (deliveryDistance <= 100) {
+            System.out.println("Доставка в пределах " + deliveryDistance + " км занимает  трое суток.");
+        }
+    }
+
+
+
+
+    public static void main (String[]args) {
+        int years = 2025;
+        leapYear(years);
+
+        int clientOs = 0;
+        int clientDeviceYear = 2016;
+        osForClientDevise(clientOs, clientDeviceYear);
+
+        int deliveryDistance = 90;
+        deliveryOfBankingProducts(deliveryDistance);
+    }
+    }
